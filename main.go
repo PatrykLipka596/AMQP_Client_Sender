7320fcd0176aecb277970154ada5855787132380
PatrykLipka596 <132571970+PatrykLipka596@users.noreply.github.com>
2023-09-10 22:27:22 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@192.168.0.153:5672/")
+	conn, err := amqp.Dial("amqp://test:test@192.168.0.153:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
